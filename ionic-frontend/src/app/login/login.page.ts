b98cad6c9e85a8ed7a726812261bb6be23360ef2
Tynamo9 <alaimo.davide1@gmail.com>
2026-05-06 01:31:49 +0200

    Pagina di login (frontend)

--- ionic-frontend/src/app/login/login.page.ts
+++ ionic-frontend/src/app/login/login.page.ts
@@ -1,22 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { IonRow, IonCol, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonButton, IonItem } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import {
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonCardTitle,
+    IonButton,
+    IonItem,
+    IonLabel,
+    IonInput,
+} from '@ionic/angular/standalone'
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonRow, IonCol, IonItem, IonCardHeader, IonCardContent, IonCardTitle, IonButton, CommonModule, FormsModule]
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+    standalone: true,
+    imports: [
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonCard,
+        IonRow,
+        IonCol,
+        IonItem,
+        IonCardHeader,
+        IonCardContent,
+        IonCardTitle,
+        IonButton,
+        CommonModule,
+        FormsModule,
+        IonLabel,
+        IonInput,
+    ],
 })
 export class LoginPage implements OnInit {
+    login() {}
+    constructor() {}
 
-  login() {}
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }

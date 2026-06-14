dc26cb42d6db06be1efc2d3087955f6a728f25e1
Tynamo9 <alaimo.davide1@gmail.com>
2026-06-15 00:00:09 +0200

    fix backendurl

--- ionic-frontend/src/app/pipes/backend-url-pipe.ts
+++ ionic-frontend/src/app/pipes/backend-url-pipe.ts
@@ -5,11 +5,14 @@ import { environment } from '../../environments/environment'
     name: 'backendUrl',
 })
 export class BackendUrlPipe implements PipeTransform {
-    private readonly baseUrl = `http://${environment.host}:${environment.port}/static`
+    private readonly baseUrl = `http://${environment.host}:${environment.port}`
 
-    transform(value: string): string {
+    transform(value: string, basepath: string = ''): string {
         if (!value) return ''
         const cleanPath = value.startsWith('/') ? value.substring(1) : value
+        if (basepath.trim().length != 0) {
+            return `${this.baseUrl}/${basepath}/${cleanPath}`
+        }
         return `${this.baseUrl}/${cleanPath}`
     }
 }
